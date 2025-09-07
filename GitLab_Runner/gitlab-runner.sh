70e847454a29b9a2dfb3f68649beada615b483fd
Andrey V. <andrey.vorobev.aqa@gmail.com>
2025-09-08 00:09:33 +0300

    Update gitlab-runner.sh

--- GitLab_Runner/gitlab-runner.sh
+++ GitLab_Runner/gitlab-runner.sh
@@ -3,7 +3,7 @@
 docker run --net gitlab_net --rm -v gitlab_runner_conf:/etc/gitlab-runner gitlab/gitlab-runner:v17.5.3 register \
   --non-interactive \
   --executor "docker" \
-  --docker-image docker:27.3.1 \
+  --docker-image docker:latest \
   --docker-network-mode="gitlab_net" \
   --docker-privileged="true" \
   --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" \
